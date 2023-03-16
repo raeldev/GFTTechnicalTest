a424f4039783707692f32cc9c57abb786b3ffb3e
Israel Moreira <israel.int@gmail.com>
2023-03-16 17:25:12 -0300

    Add Opened Cors Policy for Dev/Local running

--- TaskManager.WebAPI/Program.cs
+++ TaskManager.WebAPI/Program.cs
@@ -15,6 +15,19 @@ builder.Services.AddScoped<IKanbanTaskService, KanbanTaskService>();
 builder.Services.AddScoped<IQueueService, QueueService>();
 
 builder.Services.AddControllers();
+
+// CORS Policy for Development
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAny", 
+    policy => 
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,6 +37,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("AllowAny");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
